7690638599c60e7248ee5f38af2d30cb75d3a6a3
Bruce <bruce_rodrigues6@hotmail.com>
2013-09-17 13:35:44 -0300

    Teste da Arvore generica.

---
+++ tests/simplifiedDots/SimplifiedDotsTreeTest.java
@@ -0,0 +1,33 @@
+package simplifiedDots;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimplifiedDotsTreeTest {
+
+	@Test
+	public void test() {
+		Cell a = new Cell();
+		Cell b = new Cell();
+		Cell c = new Cell();
+		Cell d = new Cell();
+		
+		Node<Cell, Integer> na = new Node<Cell, Integer>(a);
+		Node<Cell, Integer> nb = new Node<Cell, Integer>(b);
+		Node<Cell, Integer> nc = new Node<Cell, Integer>(c);
+		Node<Cell, Integer> nd = new Node<Cell, Integer>(d);
+		
+		Tree tree = new Tree(na);
+		na.addChildren(nb);
+		nb.addChildren(nc);
+		nb.addChildren(nd);
+		tree.setRoot(nb);
+		
+		for (Node<Cell, Integer> n : na.getChildren()) {
+			assertEquals(nb, n);
+		}
+		
+	}
+
+}
