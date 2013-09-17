477475787866a02ff51af6a04a8cfc9832fbce1f
Tarcísio Fischer <tarcisio.fischer.cco@gmail.com>
2013-09-17 11:38:49 -0300

    Added board mark() method, which allows user to mark a stripe in some cell of the board.

--- src/simplifiedDots/Board.java
+++ src/simplifiedDots/Board.java
@@ -31,11 +31,61 @@ public class Board {
 	}
 	
 	/**
-	 * Get a specific cell based in its position.
-	 * 
-	 * @TODO: Maybe this method isn't really needed.
+	 * Mark a stripe in position p.
+	 * This method returns true if the stripe is marked.
+	 * It returns false if the stripe already exists or the position is invalid (invalid move).
+	 */
+	public boolean mark(Position p, Stripe s) {
+		if(!_isValidMove(p, s)) return false;
+		_cells.get(p).mark(s);
+		_markNeighbors(p, s);
+		return true;
+	}
+	
+	/**
+	 * Auxiliary method to verify if a given move (Position, Stripe) is valid.
+	 */
+	private boolean _isValidMove(Position p, Stripe s) {
+		if(!_cells.containsKey(p)) return false;
+		Cell c = _cells.get(p);
+		if(c.isFilled(s)) return false;
+		return true;
+	}
+	
+	/**
+	 * Auxiliary method to mark the neighbor cell stripe.
+	 */
+	private void _markNeighbors(Position p, Stripe s) {
+		Position pp;
+		switch(s) {
+		case UP:
+			pp = p.relativePosition(0, -1);
+			if(!_isValidMove(pp, Stripe.DOWN)) return;
+			_cells.get(pp).mark(Stripe.DOWN);
+			break;
+		case RIGHT:
+			pp = p.relativePosition(+1, 0);
+			if(!_isValidMove(pp, Stripe.LEFT)) return;
+			_cells.get(pp).mark(Stripe.LEFT);
+			break;
+		case DOWN:
+			pp = p.relativePosition(0, +1);
+			if(!_isValidMove(pp, Stripe.UP)) return;
+			_cells.get(pp).mark(Stripe.UP);
+			break;
+		case LEFT:
+			pp = p.relativePosition(-1, 0);
+			if(!_isValidMove(pp, Stripe.RIGHT)) return;
+			_cells.get(pp).mark(Stripe.RIGHT);
+			break;
+		}
+	}
+	
+	/**
+	 * Return a copy of the cell in position p.
 	 */
 	public Cell getCell(Position p) {
-		return _cells.get(p);
+		if(!_cells.containsKey(p)) return null;
+		return new Cell(_cells.get(p));
 	}
 }
