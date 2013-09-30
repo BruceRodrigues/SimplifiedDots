74c70dbb1d7da2b2f76c37b80f88448b25f3d088
Tarcísio Fischer <tarcisio.fischer.cco@gmail.com>
2013-09-30 20:43:07 -0300

    Added some improvements in IA Player

--- src/simplifiedDotsIA/IAPlayer.java
+++ src/simplifiedDotsIA/IAPlayer.java
@@ -32,6 +32,7 @@ public class IAPlayer implements Player {
 	}
 
 	public Movement getNextMovement() {
+		System.out.println("Computador pensando...");
 		if(!_movementsBuffer.isEmpty())
 			return _movementsBuffer.removeFirst();
 		
@@ -39,6 +40,7 @@ public class IAPlayer implements Player {
 			new MinMaxNode<IAGameWrapper>(new IAGameWrapper(_game));
 		_expandMinMaxTree(minMaxTree, DEPTH);
 		int maxCost = minMaxTree.getCost();
+		System.out.println("Nodos visitados =" + minMaxTree.lastVisitedNodeCount());
 		IAGameWrapper bestGameWrapper = null;
 
 		for(int i = 0; i < minMaxTree.getTotalChildren(); i++) {
@@ -97,38 +99,128 @@ public class IAPlayer implements Player {
 		LinkedList<MinMaxNode<IAGameWrapper>> possibilities =
 			new LinkedList<MinMaxNode<IAGameWrapper>>();
 		Game game = gameWrapper.game();
+		Game gameCopy = null;
+		Movement m = null;
+		
+		for(int i = 0; i < game.boardSize(); i++) {
+			gameCopy = new Game(game);
+			m = new Movement(Position.get(i, 0), Stripe.UP);
+			try {
+				Player lastPlayer = gameCopy.currentPlayer();
+				gameCopy.play(m);
+				IAGameWrapper gameCopyWrapper = new IAGameWrapper(gameCopy);
+
+				if(gameCopy.currentPlayer() == lastPlayer && !gameCopy.isOver()) {
+					List<MinMaxNode<IAGameWrapper>> nextNodes = 
+						_generatePossibilities(gameCopyWrapper);
+					for(MinMaxNode<IAGameWrapper> n : nextNodes) {
+						LinkedList<Movement> moves =
+							new LinkedList<Movement>();
+						moves.add(m);
+						moves.addAll(n.getElement().movements());
+						gameCopyWrapper.setMoves(moves);
+						possibilities.add(new MinMaxNode<IAGameWrapper>(
+								gameCopyWrapper));
+					}
+				} else {
+					LinkedList<Movement> moves = new LinkedList<Movement>();
+					moves.add(m);
+					gameCopyWrapper.setMoves(moves);
+					possibilities.add(new MinMaxNode<IAGameWrapper>(
+						gameCopyWrapper));
+				}
+
+			} catch (InvalidMovementException e) {}
+		}
+
+		for(int j = 0; j < game.boardSize(); j++) {
+			gameCopy = new Game(game);
+			m = new Movement(Position.get(0, j), Stripe.LEFT);
+			try {
+				Player lastPlayer = gameCopy.currentPlayer();
+				gameCopy.play(m);
+				IAGameWrapper gameCopyWrapper = new IAGameWrapper(gameCopy);
+
+				if(gameCopy.currentPlayer() == lastPlayer && !gameCopy.isOver()) {
+					List<MinMaxNode<IAGameWrapper>> nextNodes = 
+						_generatePossibilities(gameCopyWrapper);
+					for(MinMaxNode<IAGameWrapper> n : nextNodes) {
+						LinkedList<Movement> moves =
+							new LinkedList<Movement>();
+						moves.add(m);
+						moves.addAll(n.getElement().movements());
+						gameCopyWrapper.setMoves(moves);
+						possibilities.add(new MinMaxNode<IAGameWrapper>(
+								gameCopyWrapper));
+					}
+				} else {
+					LinkedList<Movement> moves = new LinkedList<Movement>();
+					moves.add(m);
+					gameCopyWrapper.setMoves(moves);
+					possibilities.add(new MinMaxNode<IAGameWrapper>(
+						gameCopyWrapper));
+				}
+
+			} catch (InvalidMovementException e) {}
+		}
+
 		for(int i = 0; i < game.boardSize(); i++) {
 			for(int j = 0; j < game.boardSize(); j++) {
-				for(Stripe s : Stripe.values()) {
-					Game gameCopy = new Game(game);
-					Movement m = new Movement(new Position(i, j), s);
-					try {
-						Player lastPlayer = gameCopy.currentPlayer();
-						gameCopy.play(m);
-						IAGameWrapper gameCopyWrapper = new IAGameWrapper(gameCopy);
+				gameCopy = new Game(game);
+				m = new Movement(Position.get(i, j), Stripe.RIGHT);
+				try {
+					Player lastPlayer = gameCopy.currentPlayer();
+					gameCopy.play(m);
+					IAGameWrapper gameCopyWrapper = new IAGameWrapper(gameCopy);
 
-						if(gameCopy.currentPlayer() == lastPlayer && !gameCopy.isOver()) {
-							List<MinMaxNode<IAGameWrapper>> nextNodes = 
-								_generatePossibilities(gameCopyWrapper);
-							for(MinMaxNode<IAGameWrapper> n : nextNodes) {
-								LinkedList<Movement> moves =
-									new LinkedList<Movement>();
-								moves.add(m);
-								moves.addAll(n.getElement().movements());
-								gameCopyWrapper.setMoves(moves);
-								possibilities.add(new MinMaxNode<IAGameWrapper>(
-										gameCopyWrapper));
-							}
-						} else {
-							LinkedList<Movement> moves = new LinkedList<Movement>();
+					if(gameCopy.currentPlayer() == lastPlayer && !gameCopy.isOver()) {
+						List<MinMaxNode<IAGameWrapper>> nextNodes = 
+							_generatePossibilities(gameCopyWrapper);
+						for(MinMaxNode<IAGameWrapper> n : nextNodes) {
+							LinkedList<Movement> moves =
+								new LinkedList<Movement>();
 							moves.add(m);
+							moves.addAll(n.getElement().movements());
 							gameCopyWrapper.setMoves(moves);
 							possibilities.add(new MinMaxNode<IAGameWrapper>(
-								gameCopyWrapper));
+									gameCopyWrapper));
 						}
+					} else {
+						LinkedList<Movement> moves = new LinkedList<Movement>();
+						moves.add(m);
+						gameCopyWrapper.setMoves(moves);
+						possibilities.add(new MinMaxNode<IAGameWrapper>(
+							gameCopyWrapper));
+					}
+				} catch (InvalidMovementException e) {}
 
-					} catch (InvalidMovementException e) {}
-				}
+				gameCopy = new Game(game);
+				m = new Movement(Position.get(i, j), Stripe.DOWN);
+				try {
+					Player lastPlayer = gameCopy.currentPlayer();
+					gameCopy.play(m);
+					IAGameWrapper gameCopyWrapper = new IAGameWrapper(gameCopy);
+
+					if(gameCopy.currentPlayer() == lastPlayer && !gameCopy.isOver()) {
+						List<MinMaxNode<IAGameWrapper>> nextNodes = 
+							_generatePossibilities(gameCopyWrapper);
+						for(MinMaxNode<IAGameWrapper> n : nextNodes) {
+							LinkedList<Movement> moves =
+								new LinkedList<Movement>();
+							moves.add(m);
+							moves.addAll(n.getElement().movements());
+							gameCopyWrapper.setMoves(moves);
+							possibilities.add(new MinMaxNode<IAGameWrapper>(
+									gameCopyWrapper));
+						}
+					} else {
+						LinkedList<Movement> moves = new LinkedList<Movement>();
+						moves.add(m);
+						gameCopyWrapper.setMoves(moves);
+						possibilities.add(new MinMaxNode<IAGameWrapper>(
+							gameCopyWrapper));
+					}
+				} catch (InvalidMovementException e) {}
 			}
 		}
 		
